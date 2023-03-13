35470a1190e390e2c2b95e47ea15ec586663b2ea
Akash Mahajan <akashmjn@stanford.edu>
2023-03-12 18:07:29 -0700

    apply formatting with black

--- vox/scripts/diarize_pre_sr.py
+++ vox/scripts/diarize_pre_sr.py
@@ -9,7 +9,10 @@ from tqdm import tqdm
 from pyannote.audio import Pipeline, Audio
 from pyannote.core import Segment
 from diarize_post_sr import convert_to_wav
-sys.path.append(str(Path(__file__).parent.parent.parent))  # root directory of repo, above vox
+
+sys.path.append(
+    str(Path(__file__).parent.parent.parent)
+)  # root directory of repo, above vox
 import whisper
 import whisper.utils as wutils
 
@@ -26,15 +29,17 @@ def run_pyannote_pipeline(audio_file):
     logging.info("Creating pyannote diarization pipeline ..")
     with open(TOKEN_FILE) as f:
         hf_tok = f.read().strip()
-    pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1", use_auth_token=hf_tok)
-    pipeline.to(torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
+    pipeline = Pipeline.from_pretrained(
+        "pyannote/speaker-diarization@2.1", use_auth_token=hf_tok
+    )
+    pipeline.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
 
     logging.info("Processing audio with pyannote diarization pipeline ..")
     diarization = pipeline(audio_file)
     diarized_segments = []
     for turn, _, speaker in diarization.itertracks(yield_label=True):
         s = turn.for_json()
-        s['speaker'] = speaker
+        s["speaker"] = speaker
         diarized_segments.append(s)
 
     return diarized_segments
@@ -47,17 +52,19 @@ def transcribe_cropped_segments(audio_file, diarized_segments):
     logging.info(f"Transcribing file with whisper ..")
     result = dict(text="", segments=[])
     for segment in tqdm(diarized_segments):
-        waveform, sr = pn_audio.crop(audio_file, Segment(segment['start'], segment['end']))
+        waveform, sr = pn_audio.crop(
+            audio_file, Segment(segment["start"], segment["end"])
+        )
         r = model.transcribe(waveform.squeeze().numpy(), verbose=None)
         # make all sub-segments relative to the original audio file
         for si in r["segments"]:
-            si['start'] += segment['start']
-            si['end'] += segment['end']
-            si['speaker'] = segment['speaker']
+            si["start"] += segment["start"]
+            si["end"] += segment["end"]
+            si["speaker"] = segment["speaker"]
 
-        result["text"] += r["text" ] + " "
-        result["segments"].extend(r["segments"])        
-    
+        result["text"] += r["text"] + " "
+        result["segments"].extend(r["segments"])
+
     return result
 
 
@@ -66,20 +73,24 @@ def run_pre_sr_pipeline(audio_file, output_dir):
     audio_file, _ = convert_to_wav(audio_file)
 
     # TODO@Akash - wrap this pattern into a decorator
-    diarization_result_file = Path(output_dir) / f"{Path(audio_file).name}-diarization.json"
+    diarization_result_file = (
+        Path(output_dir) / f"{Path(audio_file).name}-diarization.json"
+    )
     if not Path(diarization_result_file).is_file():
         diarized_segments = run_pyannote_pipeline(audio_file)
-        with open(diarization_result_file, 'w') as f:
+        with open(diarization_result_file, "w") as f:
             json.dump(diarized_segments, f, indent=4)
     else:
         with open(diarization_result_file) as f:
             diarized_segments = json.load(f)
 
     # summarize diarization result
-    segment_durations = [ s['end']-s['start'] for s in diarized_segments ]
-    num_speakers = len(set([s['speaker'] for s in diarized_segments]))
-    logging.info(f"Diarized into {len(segment_durations)} segments with min/median/max duration: \
-        {min(segment_durations):.2f}/{median(segment_durations):.2f}/{max(segment_durations):.2f}")
+    segment_durations = [s["end"] - s["start"] for s in diarized_segments]
+    num_speakers = len(set([s["speaker"] for s in diarized_segments]))
+    logging.info(
+        f"Diarized into {len(segment_durations)} segments with min/median/max duration: \
+        {min(segment_durations):.2f}/{median(segment_durations):.2f}/{max(segment_durations):.2f}"
+    )
     logging.info(f"Detected {num_speakers} unique speakers")
 
     # # visualize / explore diarization result
@@ -88,13 +99,13 @@ def run_pre_sr_pipeline(audio_file, output_dir):
     # # notebook.crop = Segment(0.0, 600.0)  # zoom into a region
     # notebook.crop = Segment(3000.0, 3600.0)  # zoom into a region
     # # notebook.reset()
-    
+
     # TODO@Akash - wrap this pattern into a decorator
     final_reco_file = Path(output_dir) / f"{Path(audio_file).name}.json"
     if not Path(final_reco_file).is_file():
         result = transcribe_cropped_segments(audio_file, diarized_segments)
         # with open(final_reco_file, 'w') as f:
-            # json.dump(result, f)
+        # json.dump(result, f)
     else:
         with open(final_reco_file) as f:
             result = json.load(f)
@@ -103,12 +114,18 @@ def run_pre_sr_pipeline(audio_file, output_dir):
 
 
 if __name__ == "__main__":
-
     # get arguments from command line
     import argparse
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("audio", type=str, default="../scratch/audio/earnings21-4341191.mp3")
-    parser.add_argument("output_dir", type=str, default="../scratch/transcripts/tiny.en_drzpresr/earnings21-4341191/")
+    parser.add_argument(
+        "audio", type=str, default="../scratch/audio/earnings21-4341191.mp3"
+    )
+    parser.add_argument(
+        "output_dir",
+        type=str,
+        default="../scratch/transcripts/tiny.en_drzpresr/earnings21-4341191/",
+    )
     args = parser.parse_args()
 
     os.makedirs(args.output_dir, exist_ok=True)
--- vox/scripts/run_pipelines.py
+++ vox/scripts/run_pipelines.py
@@ -15,9 +15,13 @@ import pandas as pd
 from pathlib import Path
 from diarize_post_sr import add_speakers_to_segments
 from diarize_pre_sr import run_pre_sr_pipeline
-sys.path.append(str(Path(__file__).parent.parent))         # vox directory
+
+sys.path.append(str(Path(__file__).parent.parent))  # vox directory
 from score import score_fstalign
-sys.path.append(str(Path(__file__).parent.parent.parent))  # root directory of repo, above vox
+
+sys.path.append(
+    str(Path(__file__).parent.parent.parent)
+)  # root directory of repo, above vox
 import whisper
 import whisper.utils as wutils
 
@@ -34,20 +38,34 @@ WHISPERMODEL = "tiny.en"
 
 
 if __name__ == "__main__":
-
     import argparse
+
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument("audio_file", help="path to audio file")
-    parser.add_argument("ref_file", help="path to reference transcript file in nlp format")
+    parser.add_argument(
+        "ref_file", help="path to reference transcript file in nlp format"
+    )
     parser.add_argument("output_dir", help="path to output directory")
     parser.add_argument("--num_speakers", help="number of speakers")
-    parser.add_argument("--whisper_model", help="valid whisper model name or path to checkpoint", default=WHISPERMODEL)
-    parser.add_argument("--pipelines_to_run", help="pipelines to run", default="all", choices=["all", "1.4", "1"])
+    parser.add_argument(
+        "--whisper_model",
+        help="valid whisper model name or path to checkpoint",
+        default=WHISPERMODEL,
+    )
+    parser.add_argument(
+        "--pipelines_to_run",
+        help="pipelines to run",
+        default="all",
+        choices=["all", "1.4", "1"],
+    )
     args = parser.parse_args()
 
     files_to_score = []
-    pipelines_to_run = args.pipelines_to_run.split(".") if args.pipelines_to_run != "all" else ["1", "2", "3", "4"]
-
+    pipelines_to_run = (
+        args.pipelines_to_run.split(".")
+        if args.pipelines_to_run != "all"
+        else ["1", "2", "3", "4"]
+    )
 
     # 1. transcribe audio with whisper
     logger.info("Transcribing audio with whisper ..")
@@ -55,46 +73,53 @@ if __name__ == "__main__":
     output_dir = args.output_dir
     ref_file = Path(args.ref_file).resolve()
     os.makedirs(output_dir, exist_ok=True)
-    transcribe_result_file = (Path(output_dir) / f"{Path(audio_file).name}.json").resolve()
+    transcribe_result_file = (
+        Path(output_dir) / f"{Path(audio_file).name}.json"
+    ).resolve()
     files_to_score.append((1, transcribe_result_file))
 
     if not Path(transcribe_result_file).is_file():
         model = whisper.load_model(args.whisper_model)
-        result = model.transcribe(audio_file, verbose=False, condition_on_previous_text=True, beam_size=4)
+        result = model.transcribe(
+            audio_file, verbose=False, condition_on_previous_text=True, beam_size=4
+        )
         writer = wutils.get_writer("all", output_dir)
         writer(result, audio_file)
     else:
         with open(transcribe_result_file) as f:
             result = json.load(f)
-    
 
     # 2. apply post_sr diarization
     if "2" in pipelines_to_run:
         logger.info("Applying post_sr diarization ..")
         drz_post_sr_output_dir = args.output_dir + "_drz_post_sr"
         os.makedirs(drz_post_sr_output_dir, exist_ok=True)
-        drz_post_sr_reco_file = (Path(drz_post_sr_output_dir) / f"{Path(audio_file).name}.json").resolve()
+        drz_post_sr_reco_file = (
+            Path(drz_post_sr_output_dir) / f"{Path(audio_file).name}.json"
+        ).resolve()
         files_to_score.append((2, drz_post_sr_reco_file))
 
         if not Path(drz_post_sr_reco_file).is_file():
-            result["segments"] = add_speakers_to_segments(audio_file, result["segments"], num_speakers=int(args.num_speakers))
+            result["segments"] = add_speakers_to_segments(
+                audio_file, result["segments"], num_speakers=int(args.num_speakers)
+            )
             writer = wutils.get_writer("all", drz_post_sr_output_dir)
             writer(result, audio_file)
-
 
     # 3. run pre_sr diarization and retranscribe audio
     if "3" in pipelines_to_run:
         logger.info("Running pre_sr diarization and retranscribing segmented audio ..")
         drz_pre_sr_output_dir = args.output_dir + "_drz_pre_sr"
         os.makedirs(drz_pre_sr_output_dir, exist_ok=True)
-        drz_pre_sr_reco_file = (Path(drz_pre_sr_output_dir) / f"{Path(audio_file).name}.json").resolve()
+        drz_pre_sr_reco_file = (
+            Path(drz_pre_sr_output_dir) / f"{Path(audio_file).name}.json"
+        ).resolve()
         files_to_score.append((3, drz_pre_sr_reco_file))
 
         if not Path(drz_pre_sr_reco_file).is_file():
             result = run_pre_sr_pipeline(audio_file, drz_pre_sr_output_dir)
             writer = wutils.get_writer("all", drz_pre_sr_output_dir)
             writer(result, audio_file)
-
 
     # 4. score all the results
     if "4" in pipelines_to_run:
@@ -110,10 +135,12 @@ if __name__ == "__main__":
                 # e.g. /home/whisper/vox/tiny.en/d1/f.json -> tiny.en-d1
                 result_name = "__".join(Path(reco_file).parts[-3:-1])
                 logger.info(f"Scoring {result_name} with mode {mode} ..")
-                result, _ = score_fstalign(ref_file, reco_file, result_name, speaker_turn_mode=mode)
+                result, _ = score_fstalign(
+                    ref_file, reco_file, result_name, speaker_turn_mode=mode
+                )
                 # result["method"] = f"spkturn_{mode}" if i == 1 else f"drz_post_sr" if i == 2 else f"drz_pre_sr"
                 results.append(result)
-        os.chdir(Path(__file__).parent)         # change back to vox/scripts directory
+        os.chdir(Path(__file__).parent)  # change back to vox/scripts directory
 
         results_df = pd.concat(results)
         results_df["audio_file"] = Path(audio_file).name
