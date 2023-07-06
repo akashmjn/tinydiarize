276958918213dc26cc8cc35f829aabacf9e55f78
Akash Mahajan <akash7190@gmail.com>
2023-07-06 08:49:43 +0000

    fetching precomputed finetuning data

--- tdrz_dev/scripts/fetch_finetune_stuff.sh
+++ tdrz_dev/scripts/fetch_finetune_stuff.sh
@@ -1,15 +1,28 @@
+#!/bin/sh
+
+# setup for azcopy https://aka.ms/downloadazcopy-v10-linux, much faster download than wget
+if ! command -v azcopy &> /dev/null
+then
+    echo "azcopy could not be found, installing"
+    wget https://aka.ms/downloadazcopy-v10-linux -O azcopy.tar.gz
+    tar -zxvf azcopy.tar.gz -C $PWD
+    AZCOPY_DIR=$(ls -d $PWD/azcopy_linux*)
+    export PATH=$PATH:$AZCOPY_DIR
+    echo "Fetched executable at $AZCOPY_DIR"
+fi
 
 cd ..
 
-DATADIR=/mnt/scratch
+WORKDIR=$PWD/workdir_finetune
 
-wget https://sharedstorage7190.blob.core.windows.net/tinydiarize/precomputed/tdrz_ft_ami_prepared-hf_datasets.tar.gz -C $DATADIR
-tar -zxvf $DATADIR/tdrz_ft_ami_prepared-hf_datasets.tar.gz -C $DATADIR
+mkdir -p $WORKDIR
+
+azcopy cp https://sharedstorage7190.blob.core.windows.net/tinydiarize/precomputed/tdrz_ft_ami_prepared-hf_datasets.tar.gz $WORKDIR/
+tar -zxvf $WORKDIR/tdrz_ft_ami_prepared-hf_datasets.tar.gz -C $WORKDIR
 echo "Fetched finetuning dataset"
 
-mkdir -p workdir_finetune
-wget https://sharedstorage7190.blob.core.windows.net/tinydiarize/precomputed/workdir_finetune-03_09_23.tar.gz 
-tar -zxvf workdir_finetune-03_09_23.tar.gz -C .
+azcopy cp https://sharedstorage7190.blob.core.windows.net/tinydiarize/precomputed/workdir_finetune-03_09_23.tar.gz $WORKDIR/
+tar -zxvf $WORKDIR/workdir_finetune-03_09_23.tar.gz -C $WORKDIR
 echo "Fetched finetune runs and checkpoints"
 
 cd ./scripts
